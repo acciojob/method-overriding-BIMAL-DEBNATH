771a9cd2364159661e485033800c7351ef28a8b5
BIMAL-DEBNATH <bimaldebnath8011@gmail.com>
2023-06-11 19:51:06 +0530

    method overwriding2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,15 +1,13 @@
 package com.driver;
 
- class  A{
+  class  A{
     static String meth(){
         return "Invoking method from class A";
     }
 }
 
 
- class B extends A{
-
-    public Object meth;
+class B extends A{
 
     static String meth(){
         return "Method is overridden in Extendend class B";
